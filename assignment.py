600b81a8ee40e2cb57700bd9fa04fc7bea706e75
Ariuka126 <zuulphi@gmail.com>
2026-09-21 09:16:49 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,30 +2,42 @@
 
 # Exercise 1
 def count_characters(text):
-    # Write your code here
-    pass
+    text=input("")
+    num=len(text)
+return num
 
 # Exercise 2
 def remove_spaces(text):
-    # Write your code here
-    pass
+    new_text=text.replace(" ","")
+    return new_text
 
 # Exercise 3
 def count_vowels(text):
-    # Write your code here
-    pass
-
+    count = 0
+    for i in text:
+        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+            count += 1
+    return count
 # Exercise 4
 def replace_vowels(text):
-    # Write your code here
-    pass
+    new_text = text.replace("a","*")
+    new_text = text.replace("i", "*")
+    new_text = text.replace("e", "*")
+    new_text = text.replace("o", "*")
+    new_text = text.replace("u", "*")
+    return new_text
 
 # Exercise 5
 def count_words(text):
-    # Write your code here
-    pass
-
+    count=0
+    text=text+" "
+    for i in text:
+        if i==" ":
+            count+=1
+    return count
 # Exercise 6
 def find_longest_word(text):
-    # Write your code here
-    pass
+    sentence = input("  ")
+    words = sentence.split()
+    longest_word = max(words, key=len)
+    return longest_word
